941da84ff7e221a38c7cb02b1bc74b57b7c3252b
LoopingStudio <kelian.daste31@gmail.com>
2022-06-22 13:46:59 +0200

    Initial Commit

---
+++ DemoFirebaseAFPE/DemoFirebaseAFPEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoFirebaseAFPEApp.swift
+//  DemoFirebaseAFPE
+//
+//  Created by Kelian Daste on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoFirebaseAFPEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
